d712ce70a96f14d75b756a426b8066a326f00839
Mayur Rithe <mayurrithe2004@gmail.com>
2026-06-19 16:07:58 +0530

    Commited

--- server/server.js
+++ server/server.js
@@ -16,7 +16,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://e-commerce-website-lnms.onrender.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 
